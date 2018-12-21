30b9a6e94818210217274863e618a89cafcdf2be
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2018-12-21 15:42:31 +0000

    Add console.log telling you when the app is running

--- src/lib/index.js
+++ src/lib/index.js
@@ -58,7 +58,8 @@ const port = 3000;
 app.listen(port, () => {
     // https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
     // https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-    // console.log(
-    //     `\x1b[32m--- CICA Online module app listening on \x1b[4m${port}\x1b[24m ---\x1b[0m`
-    // );
+    // eslint-disable-next-line no-console
+    console.log(
+        `\x1b[32m--- CICA Online module app listening on \x1b[4m${port}\x1b[24m ---\x1b[0m`
+    );
 });
